d3fd2bd3b72c9967dd7f15802d88990164aa30e7
Stan Saner <stan.saner@hotmail.co.uk>
2022-12-15 20:22:10 +0000

    completed jquery version

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 /* Variables */
 
-var searchInput = document.querySelector('.search');
-var cardWrapper = document.querySelector('main');
+var searchInput = $('.search');
+var cardWrapper = $('main');
 
 function noMatch() {
-  cardWrapper.innerHTML = '<p class="no-search">No results found.</p>';
+  cardWrapper.html('<p class="no-search">No results found.</p>');
 }
 
 function displayMatches(matches) {
-  cardWrapper.innerHTML = '';
+  cardWrapper.html('');
 
   if (!matches) {
     noMatch();
   } else {
     for (var matchObj of matches) {
-      cardWrapper.insertAdjacentHTML('beforeend', `
+      cardWrapper.append(`
       <div class="movie-card" style="background-image: 
         linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), 
         url(${matchObj.Poster});">
@@ -31,29 +31,41 @@ function displayMatches(matches) {
 
 function fetchMovies(event) {
   var keyCode = event.keyCode;
-  var searchText = searchInput.value.toLowerCase().trim();
+  // toLowerCase and trim() are javascript programming language 
+  // functions that operate on strings. The val() method returns 
+  // a string, so it will work
+  // toLowerCase() isn't actually needed as val() is case insensitive
+  var searchText = searchInput.val().toLowerCase().trim();
+
+  // in jquery no argument passed gets
+  // argument passed in sets
 
   if (keyCode === 13 && searchText) {
 
-    var responsePromise = fetch(`https://www.omdbapi.com/?apikey=6aea06d6&s=${searchText}`);
+    // var responsePromise = fetch(`https://www.omdbapi.com/?apikey=6aea06d6&s=${searchText}`);
+    $.get(`https://www.omdbapi.com/?apikey=6aea06d6&s=${searchText}`)
+        .then(function (data) {
+            displayMatches(data.Search);
+            searchInput.val('');
+        });
 
-    function handleResponse(responseObj) {
-      return responseObj.json();
-    }
+    // function handleResponse(responseObj) {
+    //   return responseObj.json();
+    // }
 
-    responsePromise
-      .then(handleResponse)
-      .then(function (data) {
-        displayMatches(data.Search);
-        searchInput.value = '';
-      });
+    // responsePromise
+    //   .then(handleResponse)
+    //   .then(function (data) {
+        // displayMatches(data.Search);
+        // searchInput.value = '';
+    //   });
 
 
   }
 }
 
 function init() {
-  searchInput.addEventListener('keydown', fetchMovies);
+  searchInput.keydown(fetchMovies);
 }
 
 init();
